6b911c657dbb5e8879989980c7cd8dd85c38d0c9
SchoolMakerDay <50214407+SchoolMakerDay@users.noreply.github.com>
2025-03-20 10:21:02 +0100

    Add files via upload

---
+++ views/firstSection.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <!--
+            *  @date 09 02 2023
+            *  @author Giovanni Ragno
+            *  @copyright https://creativecommons.org/licenses/by-sa/4.0/
+			*  @date 24 02 2023
+			*  @author Duilio Peroni
+			*  @added logo,name and abstract
+
+        -->
+        <title><?= $title ?></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="vendor/jquery/jquery-3.6.0.min.js"></script>
+    </head>
+    <body>
+        <div id="header">
+		<h1 style="height:120px !important"><a href="index.php"><img height="120px" src="img/logo_rabbits.png" alt="Rabbits: A Basic Broker for Internet of ThingS"></a></h1>
+                <h1 style="clear:left">Rabbits</h1>
+	</div>
+        <div id="content">
+            <h2 class="main"><?= $title ?></h2>
---
+++ views/lastSection.php
@@ -0,0 +1,9 @@
+        </div>
+        <div id="footer">
+		<p>
+                    Realizzato per <a href="https://www.schoolmakerday.it/">SchoolMakerDay</a> 2023<br>
+                    Copyleft <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>
+                </p>
+        </div>
+    </body>
+</html>
