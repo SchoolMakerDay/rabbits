f854c3ba808fb865f794f460227315eb2b2dac91
SchoolMakerDay <50214407+SchoolMakerDay@users.noreply.github.com>
2025-03-20 06:16:29 +0100

    Add files via upload

---
+++ dashboard/service-worker.js
@@ -0,0 +1,29 @@
+var cacheName = 'pwa-dashboard';
+var filesToCache = [
+'/rabbits/dashboard/index.html'
+];
+/*
+var filesToCache = [
+'/rabbits/dashboard/',
+'/rabbits/dashboard/index.html',
+'/rabbits/dashboard/css/stylesheet.css',
+'/rabbits/dashboard/images/pwa-logo.svg',
+'/rabbits/dashboard/js/core.js'
+];
+*/ 
+/* Avvia il Service Worker e Memorizza il contenuto nella cache */
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+}); 
+/* Serve i Contenuti Memorizzati quando sei Offline */
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
