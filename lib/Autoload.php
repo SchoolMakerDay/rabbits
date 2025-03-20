b322b04c64eb4318bb34e1bf09c59b8e1852bf9d
SchoolMakerDay <50214407+SchoolMakerDay@users.noreply.github.com>
2025-03-20 09:54:21 +0100

    Add files via upload

---
+++ lib/Autoload.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace lib;
+
+
+/**
+ * Description of Autoload
+ *
+ * @author gianni
+ */
+class Autoload {
+    
+        /**
+         * registra la funzione di autoload
+         */
+	public static function autoload()
+	{
+		spl_autoload_register('self::loader');
+	}
+	
+        /**
+         * include il file con la dichiarazione della classe
+         * @param string $class
+         */
+	public static function loader(string $class)
+	{
+		require str_replace('\\', '/', $class).'.php'; 
+	}
+
+}
